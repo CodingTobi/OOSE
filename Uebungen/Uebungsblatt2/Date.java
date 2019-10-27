a0553b76cb46309ce7c36e13bcd96da450b6c224
CodingTobi <tobi.sassin@gmail.com>
2019-10-27 18:06:35 +0100

    Fertig aber noch nicht gestestet!

--- Uebungen/Uebungsblatt2/Date.java
+++ Uebungen/Uebungsblatt2/Date.java
@@ -1,3 +1,20 @@
 class Date {
-  int.java
+	private int day;
+	private int month;
+	private int year;
+
+	public Date(int day, int month, int year) {
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
+
+	public String toString() {
+		return day + "." month + "." year;
+	}
+
+	public static void main(String[] args) {
+		Date d1 = new Date(30, 11, 2018);
+		System.out.println(d1.toString());
+	}
 }
