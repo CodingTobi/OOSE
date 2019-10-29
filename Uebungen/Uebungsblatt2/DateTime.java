95f2351385263c811559c758eff777bd0798f51e
CodingTobi <amnesia@localhost>
2019-10-29 11:08:22 +0000

    DateTime fertig gemacht --- noch nicht getestet.

--- Uebungen/Uebungsblatt2/DateTime.java
+++ Uebungen/Uebungsblatt2/DateTime.java
@@ -2,8 +2,18 @@ class DateTime {
   private Date d;
   private Time t;
 
-public DateTime(Date d, Time t) {
-  this.d = d;
- this.t = t;
-}
+  public DateTime(Date d, Time t) {
+    this.d = d;
+    this.t = t;
+  }
+
+  public string toString() {
+    System.out.println(d.toString() + " " + t.toString());
+  }
+
+  public static void main (String[] args) {
+    Date d = new Date(29,11,2018);
+    Time t = new Time(12,59,15);
+    DateTime dt = new DateTime(d, t);
+  }
 }
