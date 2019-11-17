1c33ed617555782e912d7c59ef2dc1f73d232773
CodingTobi <tobi.sassin@gmail.com>
2019-11-17 20:23:12 +0100

    jetzt müsste es aktuell sein

--- Uebungen/Uebungsblatt4/Heron.java
+++ Uebungen/Uebungsblatt4/Heron.java
@@ -1,9 +1,17 @@
-class Heron{
-  static double sqrt(double x){
-    assert x>=0;
-    return sqrt(x , (x + 1) / 2) ;
-  }
-  static double sqrt(double x, double approx) {
-      return (n> 1/2*(sqrt(x-1, approx))
-  }
+package Uebungsblatt4;
+
+class Heron {
+	static double sqrt(double x) {
+		assert x >= 0;
+		return sqrt(x, (x + 1) / 2);
+	}
+
+	static double sqrt(double x, double approx) {
+		return (approx == (double) 1 / 2 * (approx + x / approx)) ? approx
+				: sqrt(x, (double) 1 / 2 * (approx + x / approx));
+	}
+
+	public static void main(String[] args) {
+		System.out.println(sqrt(9));
+	}
 }
