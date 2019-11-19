c66fa2a083030a4d1bc6b8d1c0566106e31148cf
CodingTobi <tobi.sassin@gmail.com>
2019-11-19 15:40:57 +0100

    Geometric fertig, sollte gehn

---
+++ Uebungen/Uebungsblatt5/GeometricObject.java
@@ -0,0 +1,51 @@
+package Uebungsblatt5;
+
+public class GeometricObject {
+	Vertex corner;
+	double width;
+	double height;
+	Vertex velocity;
+
+	GeometricObject(Vertex corner, double width, double height,
+			Vertex velocity) {
+		this.corner = corner;
+		this.width = width;
+		this.height = height;
+		this.velocity = velocity;
+	}
+
+	double size() {
+		return width * height;
+	}
+
+	boolean isLargerThan(GeometricObject that) {
+		return this.size() > that.size();
+	}
+
+	boolean isAbove(GeometricObject that) {
+		return that.corner.y > (this.height + this.corner.y);
+	}
+
+	boolean isUnderneath(GeometricObject that) {
+		return this.corner.y > (that.height + that.corner.y);
+	}
+
+	boolean isLeftOf(GeometricObject that) {
+		return (this.corner.x - this.width) < that.corner.x;
+	}
+
+	boolean isRightOf(GeometricObject that) {
+		return (that.corner.x + that.width) < this.corner.x;
+	}
+
+	boolean touches(GeometricObject that) {
+		return (!isRightOf(that) && !isLeftOf(that)
+				|| !isAbove(that) && !isUnderneath(that));
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Uebungen/Uebungsblatt5/Vertex.java
@@ -0,0 +1,25 @@
+package Uebungsblatt5;
+
+public class Vertex {
+	double x;
+	double y;
+
+	Vertex(double x, double y) {
+	    this.x = x;
+	    this.y = y;
+	  }
+
+	void move(Vertex v) {
+		x += v.x;
+		y += v.y;
+	}
+
+	void moveTo(Vertex v) {
+		x = v.x;
+		y = v.y;
+	}
+
+	public String toString() {
+		return "(" + x + ", " + y + ")";
+	}
+}
