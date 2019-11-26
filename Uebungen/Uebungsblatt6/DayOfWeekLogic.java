be2ad5b814a7383867e82dc0373b55eaf1dd6fd7
CodingTobi <tobi.sassin@gmail.com>
2019-11-26 10:34:19 +0100

    Angefangen, in OOSE weiter

---
+++ Uebungen/Uebungsblatt6/DayOfWeekLogic.java
@@ -0,0 +1,21 @@
+package Uebungsblatt6;
+
+class DayOfWeekLogic extends ButtonLogic {
+
+	public static void main(String[] args) {
+		// new Dialogue(new DayOfWeekLogic());
+		new ConsoleDialogue(new DayOfWeekLogic()).run();
+	}
+
+	@Override
+	String getButtonLabel() {
+		
+		return "";
+	}
+
+	@Override
+	String eval(String x) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
