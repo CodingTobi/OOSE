87eb79e7d5d167bf38e79cfa2c1691f615eecdf8
CodingTobi <tobi.sassin@gmail.com>
2019-11-26 19:33:30 +0100

    Fertig und abgegeben 20/20

--- Uebungen/Uebungsblatt6/DayOfWeekLogic.java
+++ Uebungen/Uebungsblatt6/DayOfWeekLogic.java
@@ -14,9 +14,27 @@ class DayOfWeekLogic extends ButtonLogic {
 
 	@Override
 	String eval(String x) {
-		String[] temp = x.split(".");
-		System.out.println(Integer.valueOf(temp[0]));
-		return "" + new Date(Integer.valueOf(temp[0]),
-				Integer.valueOf(temp[1]), Integer.valueOf(temp[2])).dayOfWeek();
+		String[] temp = x.split("\\.");
+		int dayNr = new Date(Integer.valueOf(temp[0]), Integer.valueOf(temp[1]),
+				Integer.valueOf(temp[2])).dayOfWeek();
+		switch (dayNr) {
+			case 0:
+				return "Sonntag";
+			case 1:
+				return "Montag";
+			case 2:
+				return "Dienstag";
+			case 3:
+				return "Mittwoch";
+			case 4:
+				return "Donnerstag";
+			case 5:
+				return "Freitag";
+			case 6:
+				return "Sonnabend";
+			default:
+				return "Error";
+		}
+		
 	}
 }
